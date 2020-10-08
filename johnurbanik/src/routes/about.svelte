cc51855acd1cf9dfb793d6e4f6c0c79b45bfa61c
John Urbanik <johnurbanik@gmail.com>
2020-10-08 19:02:22 -0400

    About

--- johnurbanik/src/routes/about.svelte
+++ johnurbanik/src/routes/about.svelte
@@ -1,5 +1,5 @@
 <style>
-  figure {
+  /* figure {
     float: right;
     margin: -3em -7em 2em 2em;
     max-width: 400px;
@@ -14,23 +14,31 @@
       float: none;
       margin: 0 auto 2em;
     }
-  }
+  } */
 </style>
 
 <svelte:head>
-  <title>About</title>
+  <title>John Urbanik - About</title>
 </svelte:head>
 
 <div class="container">
-  <h1>About</h1>
-  <figure>
-    <img src='rsz_florian-klauer-489-unsplash.jpg' alt='Vintage typewriter.'>
-    <figcaption>Photo by <a href="https://unsplash.com/@florianklauer" target="_blank">Florian Klauer</a> on Unsplash</figcaption>
-  </figure>
-  <p>Text placeholder via <a href="https://jeffsum.com/" target="_blank">Jeffsum</a>.</p>
-  <p>So you two dig up, dig up dinosaurs? What do they got in there? King Kong? My dad once told me, laugh and the world laughs with you, Cry, and I'll give you something to cry about you little bastard! Life finds a way. God creates dinosaurs. God destroys dinosaurs. God creates Man. Man destroys God. Man creates Dinosaurs.</p>
-  <p>You really think you can fly that thing? You know what? It is beets. I've crashed into a beet truck. Forget the fat lady! You're obsessed with the fat lady! Drive us out of here! Is this my espresso machine? Wh-what is-h-how did you get my espresso machine?</p>
-  <p>Hey, you know how I'm, like, always trying to save the planet? Here's my chance. Hey, take a look at the earthlings. Goodbye! I was part of something special. Just my luck, no ice. You're a very talented young man, with your own clever thoughts and ideas. Do you need a manager?</p>
-  <p>Jaguar shark! So tell me - does it really exist? This thing comes fully loaded. AM/FM radio, reclining bucket seats, and... power windows. Yes, Yes, without the oops! You're a very talented young man, with your own clever thoughts and ideas. Do you need a manager?</p>
-  <p>Yes, Yes, without the oops! Do you have any idea how long it takes those cups to decompose. They're using our own satellites against us. And the clock is ticking. Do you have any idea how long it takes those cups to decompose. My dad once told me, laugh and the world laughs with you, Cry, and I'll give you something to cry about you little bastard!</p>
+  <h2>About John</h2>
+
+  <p>I am a mission-driven and ethically conscious data scientist and engineer.
+  I focus on building end-to-end systems that help humans solve difficult, open-ended problems.
+  I'm particularly passionate about the development of systems that promote equity while enabling new workflows, insights and experiences.</p>
+  <p>Over the course of my career, I've demonstrated strong intuition and the
+  ability to dial into details while keeping hte big picture in sight. I'm adept at switching
+  contexts between research, applied data science, data engineering, people management, and strategy work.</p>
+
+  <p>As a manager and practitioner, I place a strong emphasis on exploratory data analysis, explainability,
+  reliable data pipelines, and engaging stakeholders/users in development process. I value communication,
+  transparency, team, and mission.</p>
+
+  <p>My primary tech stack is the Python data stack, and methods from complexity science, causal inference, and Bayesian statistics resonte deeply with me.
+  However, I'm very comfortable learning new technologies and techniques on the fly.</p>
+
+  <p>Outside of work, I am passionate about all things music/musicology, the anthropology of food (and eating/cooking it), mycology, sustainability, and ethics.</p>
+
+  <p>My resume is available <a href="jurbanik2020short.pdf">here</a>.</p>
 </div>
